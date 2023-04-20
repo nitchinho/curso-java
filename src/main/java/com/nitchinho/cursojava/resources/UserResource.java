7f9c97dbed244e7b0992989f36b091d702b763f6
nitchinho <nitchinho007@gmail.com>
2023-04-19 21:33:42 -0300

    Initial commit

---
+++ src/main/java/com/nitchinho/cursojava/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.nitchinho.cursojava.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nitchinho.cursojava.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1, "Maria", "maria@gmail.com", "11964290237", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
