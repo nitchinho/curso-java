07a815aad55e4ab56ba7f28c8bfc6598d80bcfad
nitchinho <nitchinho007@gmail.com>
2023-04-28 20:23:42 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/nitchinho/cursojava/config/TestConfig.java
+++ src/main/java/com/nitchinho/cursojava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nitchinho.cursojava.entities.Category;
 import com.nitchinho.cursojava.entities.Order;
 import com.nitchinho.cursojava.entities.OrderItem;
+import com.nitchinho.cursojava.entities.Payment;
 import com.nitchinho.cursojava.entities.Product;
 import com.nitchinho.cursojava.entities.User;
 import com.nitchinho.cursojava.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
